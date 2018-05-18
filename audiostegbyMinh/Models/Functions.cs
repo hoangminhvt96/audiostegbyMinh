ec70c603eabb6356f3aabfb9eafc865879057d04
Hoang Minh <31468445+hoangminhvt96@users.noreply.github.com>
2018-05-18 21:33:29 +0700

    Add project files.

---
+++ audiostegbyMinh/Models/Functions.cs
@@ -0,0 +1,22 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace audiostegbyMinh.Models
+{
+    public class Functions
+    {
+        public static void Mp3ToWav(string mp3File, string outputFile)
+        {
+            using (Mp3FileReader reader = new Mp3FileReader(mp3File))
+            {
+                using (WaveStream pcmStream = WaveFormatConversionStream.CreatePcmStream(reader))
+                {
+                    WaveFileWriter.CreateWaveFile(outputFile, pcmStream);
+                }
+            }
+        }
+    }
+}
---
+++ audiostegbyMinh/Models/GoogleDriveFiles.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace audiostegbyMinh.Models
+{
+    public class GoogleDriveFiles
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public long? Size { get; set; }
+        public long? Version { get; set; }
+        public DateTime? CreatedTime { get; set; }
+        public string PathFile { get; set; }
+        public string MimeType { get; set; }
+    }
+}
